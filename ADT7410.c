c7d3e75683ca23e9e9f70cd8d78d12628270f577
OKAMURA Yasunobu <okamura@informationsea.info>
2014-12-23 22:47:30 +0900

    initial commit

---
+++ ADT7410.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    int file;
+    int adapter_nr = 1;
+    int addr = 0x48;
+    char filename[50];
+
+    snprintf(filename, sizeof(filename)-1, "/dev/i2c-%d", adapter_nr);
+    file = open(filename, O_RDWR);
+    if (file < 0) {
+        perror("Cannot open i2c device");
+        return 1;
+    }
+
+    if (ioctl(file, I2C_SLAVE, addr) < 0) {
+        perror("Cannot set slave address");
+        close(file);
+        return 1;
+    }
+
+/*
+    if (ioctl(file, I2C_RDWR, 1) < 0) {
+        perror("Cannot set RDWR");
+        close(file);
+        return 1;
+    }
+*/
+
+    uint8_t reg = 0;
+    char buf[256];
+    ssize_t length;
+    if (write(file, &reg, 1) < 0) {
+        perror("Cannot write to i2c device");
+        goto onexit;
+    }
+
+    if ((length = read(file, buf, 4)) < 0) {
+        perror("Cannot read from i2c device");
+        goto onexit;
+    }
+
+/*
+    printf("length: %d\n", length);
+
+    {
+        ssize_t i;
+        for (i = 0; i < length; i++) {
+            printf("[%02x] %02x\n", i, buf[i]);
+        }
+    }
+*/
+    if ((buf[2] & 0x80) == 0) {
+        fprintf(stderr, "Not ready.\n");
+        goto onexit;
+    }
+    int value = (buf[0] << 8) | buf[1];
+    printf("%lf\n", ((double)value)/16/8);
+    
+
+  onexit:    
+    close(file);
+
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+ADT7410 Temperature Reader for Linux
+====================================
+
+How to use
+----------
+
+```
+git clone https://github.com/informationsea/ADT7410.git
+cd ATD7410
+make
+sudo chown root:root ADT7410
+sudo chmod gu+s ADT7410
+./ADT7410
+```
+
+Tested Devices & Software
+--------------
+
+* Raspberry Pi Model B+
+* Arch Linux
+
+License
+-------
+
+Copyright (C) 2014 OKAMURA, Yasunobu
+
+GPL v3 or later
+
+
